a99e496a552d7d5f6f7a7d1639edabb1a11f2af3
KatrineVedel <katr499k@stud.kea.dk>
2022-03-06 15:02:14 +0100

    dying slowly, but im a survivor

--- exercises/hog/script.js
+++ exercises/hog/script.js
@@ -1,16 +1,19 @@
+//Mathilde and Katrine have been working together with this assignment, therefore there is similarities in both codes, but its made individually
+
 "use strict";
 
 window.addEventListener("DOMContentLoaded", start);
 
 //Template object for student
 const Student ={
+    fullname: "",
     firstname: "",
     middlename: "",
     nickname: "",
     lastname: "",
     gender: "",
     house: "",
-    bloodtype: "",
+    blood: "",
     image: ""
 }
 
@@ -20,20 +23,30 @@ let allStudents = [];
 //Variable for the filter function
 let filteredStu;
 
+//Function that starts the whole systaaaaam
 function start(){
     console.log("start");
 
-    //Here I add eventlistener for all my filter buttons
-     document.querySelectorAll("[data-action='filter']").forEach((btn) => {
-        btn.addEventListener("click", studentClick);
-    });
-    
     //Here I call functions
     loadJSON();
+    registerButtons();
 
 }
 
+function registerButtons(){
+   
+    //Here I add eventlistener for all my filter buttons
+    document.querySelectorAll("[data-action='filter']").forEach((btn) => {
+        btn.addEventListener("click", studentClick);
+    });
+    //Here I add eventlistener for my searchbar button
+    document.querySelector("#searchField").addEventListener("input", searchInput);
+    
+}
 
+
+
+//Here is the event and if statement for the filterfunction
  function studentClick(evt){
     const myFilter = evt.target.dataset.filter;
     if (myFilter === "*"){
@@ -46,6 +59,7 @@ function start(){
     console.log("Yo", evt.target.dataset.filter);
 }
 
+//Here is my filter function for each houses
 function isStudent(house){
     console.log("House", house);
 
@@ -61,9 +75,7 @@ function isStudent(house){
     displayList(list);
 }
 
-
-
-//Here I fetch the data for both json files
+//Here I fetch the data for both json files for students and bloodstatus
 async function loadJSON() {
     //Json over students
     const response = await fetch('https://petlatkea.dk/2021/hogwarts/students.json');
@@ -83,7 +95,7 @@ async function loadJSON() {
 }
 
 //Preparing the array and implementing it in the template Student and adding new variables
-    function preparedObjects(jsonData, jsonDataB){
+    function preparedObjects(jsonData){
 
         jsonData.forEach(jsonObject => {
 
@@ -94,11 +106,11 @@ async function loadJSON() {
             let originalName = jsonObject.fullname.trim();
             //Here I am splitting the string into array, so I can define variables for each index.
             originalName = originalName.split(" ");
+            let fullName;
             let firstname = originalName[0].charAt(0).toUpperCase() + originalName[0].substring(1).toLowerCase();
             let middlename;
             let lastname;
-            let nickname;
-
+            let nickname;       
             let gender = jsonObject.gender;
             let house = jsonObject.house.trim();
             
@@ -108,23 +120,25 @@ async function loadJSON() {
             //Making an if statement, so if the length of the splittet array is equal to 1, then lastname is the middlename and the other way around
             if(originalName.length === 2){
                 lastname = originalName[1].charAt(0).toUpperCase() + originalName[1].substring(1).toLowerCase();
+                fullName = firstname + " " + lastname;
                
             } else if (originalName.length === 3){
                middlename = originalName[1].charAt(0).toUpperCase() + originalName[1].substring(1).toLowerCase();
                lastname =  originalName[2].charAt(0).toUpperCase() + originalName[2].substring(1).toLowerCase();
+               fullName = firstname + " " + middlename + " " + lastname;
           
-if(middlename.includes(`"`)){
-    middlename = undefined;
-    nickname = originalName[1];
-    //Here I remove the "" from the nicknames and make them set to Uppercase at first letter
-    let firstQuotationmark = nickname.indexOf(`"`);
-    let lastQuotationmark = nickname.lastIndexOf(`"`);
-    nickname = nickname.substring(firstQuotationmark + 1, lastQuotationmark).charAt(0).toUpperCase() + nickname.substring(firstQuotationmark + 1, lastQuotationmark).substring(1).toLowerCase();
-}
+            if(middlename.includes(`"`)){
+                middlename = undefined;
+                nickname = originalName[1];
+                //Here I remove the "" from the nicknames and make them set to Uppercase at first letter
+                let firstQuotationmark = nickname.indexOf(`"`);
+                let lastQuotationmark = nickname.lastIndexOf(`"`);
+                nickname = nickname.substring(firstQuotationmark + 1, lastQuotationmark).charAt(0).toUpperCase() + nickname.substring(firstQuotationmark + 1, lastQuotationmark).substring(1).toLowerCase();
+            }
 
             }
 
-
+            //Function to get the images to each student
             function getImage(firstname, lastname) {
                 let image
                 if(lastname == "Patil"){
@@ -141,6 +155,11 @@ if(middlename.includes(`"`)){
                 student.image = getImage(firstname, lastname);
               } else student.image = null;
             
+              if (lastname) {
+                student.image = getImage(firstname, lastname);
+                } else student.image = "images/null.png";
+    
+
 
             //Here I declare it so it fits to the template and make sure to join the fullname
             student.firstname = firstname;
@@ -150,76 +169,124 @@ if(middlename.includes(`"`)){
             student.gender = gender.charAt(0).toUpperCase() + gender.substring(1).toLowerCase();
             student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();  
             
-            if (lastname) {
-            student.image = getImage(firstname, lastname);
-            } else student.image = "images/null.png";
-
-            
 
             //Here I push my array into the student template
             allStudents.push(student); 
            
         });
 
+        //Calling my functions
         displayList(allStudents);
         countingStudent(allStudents);
 
 
     }
 
-
-    function bloodType(addBlood){
+    //Here I make a function saying for each student with if statements determine wether a student is half, pure or muggle
+    function bloodType(addBlood) {
         const pureblood = addBlood.pure;
         const halfblood = addBlood.half;
-
+    
         allStudents.forEach((student) => {
             pureblood.forEach((pure) => {
-                if(student.lastname === pure){
-                    student.blood = "pure";
-                } else {
+                if (student.lastname === pure) {
+                    student.blood = "Pureblood 🩸";
+                } else
                     halfblood.forEach((half) => {
-                        if (student.lastname === half){
-                            student.blood = "half";
+                        if (student.lastname === half) {
+                            student.blood = "Halfblood ✨";
                         }
                     });
-                }
             });
-            if(!student.blood) {
-                student.blood = "Muggle";
+    
+            if (!student.blood) {
+                student.blood = "Muggle 👀";
             }
             console.log(student);
         });
-
+    displayList(allStudents);
     }
 
-//Here is my displaylist function where I copy my array into the displaystudent function
+    //Here is my displaylist function where I copy my array into the displaystudent function
      function displayList(students){
         //Clear the list
       document.querySelector("#student_list").innerHTML = "";
         //Build a new list
-     students.forEach(displayStudent);
+      students.forEach(displayStudent);
     }
 
+    function addStyles(studenthouse, dest) {
+        dest.className = "";
+        dest.classList.add(studenthouse);
+    }
+    
 
     //Here I display all the students in the article/list
     function displayStudent(student){
         //Creating a clone
         const clone = document.querySelector("#student_template").content.cloneNode(true);
+        const popUpArticle = clone.querySelector("#studentArticle");
         
-        // set clone data
-        clone.querySelector("[data-field=firstname]").textContent = "Firstname: " + student.firstname;
-        clone.querySelector("[data-field=nickname]").textContent = "Nickname: " + student.nickname;
-        clone.querySelector("[data-field=middlename]").textContent = "Middlename: " + student.middlename;
-        clone.querySelector("[data-field=lastname]").textContent = "Lastname: " + student.lastname;
-        clone.querySelector("[data-field=gender]").textContent = "Gender: " + student.gender;
+        // set clone data and showing full name, image and house on the list
+        clone.querySelector("[data-field=fullname]").textContent = `${student.firstname} ${student.middlename} ${student.lastname}`;
         clone.querySelector("[data-field=house]").textContent = "House: " + student.house;
         clone.querySelector(".image").src = student.image;
-       // clone.querySelector(".image").src = "images/" + student.lastname.substring(student.lastname.lastIndexOf(""), student.lastname.indexOf("-") + 1).toLowerCase() + "_" + student.firstname.substring(0, 1).toLowerCase() + ".png";
+
+        if (!student.middlename) {
+            clone.querySelector("[data-field=fullname]").textContent = `${student.firstname} ${student.lastname}`;
+        }
+        if (!student.lastname) {
+            clone.querySelector("[data-field=fullname]").textContent = student.firstname;
+        }
+
+
+
+
+       
+        //Adding an eventlistener for the popup window
+         popUpArticle.addEventListener("click", () => showDetails(student));
 
         // append clone to list
         document.querySelector("#student_list").appendChild( clone );
 
     } 
+
+//Here I make my search function
+function searchInput(evt){
+    console.log("Search button works");
+    
+    displayList(
+        allStudents.filter((elm) => {
+          return elm.firstname.toUpperCase().includes(evt.target.value.toUpperCase()) || elm.lastname.toUpperCase().includes(evt.target.value.toUpperCase());
+        })
+      );
+
+}
+
+    //Here is my function for showing details for the popup window
+    function showDetails(student){
+        const popup = document.querySelector("#popup");
+
+    popup.querySelector("[data-field=firstname]").textContent = "Firstname: " + student.firstname;
+    popup.querySelector("[data-field=nickname]").textContent = "Nickname: " + student.nickname;
+    popup.querySelector("[data-field=middlename]").textContent = "Middlename: " + student.middlename;
+    popup.querySelector("[data-field=lastname]").textContent = "Lastname: " + student.lastname;
+    popup.querySelector("[data-field=gender]").textContent = "Gender: " + student.gender;
+    popup.querySelector("[data-field=blood]").textContent = "Bloodtype: " + student.blood;
+    popup.querySelector(".crest").src = `crest/${student.house}.png`;
+
+    //set display style to block to make visible
+    popup.style.display = "block";
+
+
+    
+
+    //Here I add event with click to close the popup window
+    document.querySelector("#close").addEventListener("click", () => popup.style.display = "none");
+    }
+
+    //Here I call my style function so every house gets a theme at the popup
+    addStyles(student.house, detailArticle);
 
     //Here is my counter function where I show how many students in every house and total
     function countingStudent(student){
@@ -227,6 +294,7 @@ if(middlename.includes(`"`)){
         //Here I call the counter for the students
         displayCountingStudent(totalStudents(), gryffindorStudent(), slytherinStudent(), hufflepuffStudent(), ravenclawStudent());
 
+        //Here I make the functions that I call for the counter
         function totalStudents(){
             return student.length;
         }
@@ -270,8 +338,9 @@ if(middlename.includes(`"`)){
         
     }
 
+    //Here I display all the numbers of the students and houses
     function displayCountingStudent(total, hufflepuffNumber, ravenclawNumber, gryffindorNumber, slytherinNumber, expelled) {
-        document.querySelector(".total").textContent += total;
+        document.querySelector(".total").textContent += " " + total;
         document.querySelector(".hpStudents").textContent += " " + hufflepuffNumber;
         document.querySelector(".rcStudents").textContent += " " + ravenclawNumber;
         document.querySelector(".gdStudents").textContent += " " + gryffindorNumber;
